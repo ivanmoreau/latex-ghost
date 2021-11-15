4d5a297e352ae79079718573c34afa52b4a1177e
Ivan Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2021-11-15 05:56:26 -0600

    This is a mess.

---
+++ latexcss.sh
@@ -0,0 +1,7 @@
+cp node_modules/latex.css/style.min.css assets/built/latex.min.css
+cp node_modules/katex/dist/katex.min.js assets/built/katex.min.js
+cp node_modules/katex/dist/katex.min.css assets/built/katex.min.css
+cp node_modules/katex/dist/fonts/* assets/built/fonts/
+cp node_modules/katex/dist/contrib/copy-tex.min.js assets/built/copy-tex.min.js
+cp node_modules/katex/dist/contrib/copy-tex.min.css assets/built/copy-tex.min.css
+cp node_modules/katex/dist/contrib/auto-render.min.js assets/built/auto-render.min.js
